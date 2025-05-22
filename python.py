bf920fc98096bd88ee2671f01e3797406d775bd3
Enzo9790 <99890071+Enzo9790@users.noreply.github.com>
2025-05-22 17:45:58 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,10 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=== RESTART: C:/Users/aluno/AppData/Local/Programs/Python/Python313/python.py ==
+Numero 1 :24
+Numero 2 :46
+Resultado soma 70.0
+Resultado subtração -22.0
+Resultado multiplicação 1104.0
+Resultado divisão 0.5217391304347826
